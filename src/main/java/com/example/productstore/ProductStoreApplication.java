79bfdd3d80d95a67696212b5d2fba6cc2489a712
Test <matonikolov77@yahoo.com>
2023-01-15 21:24:48 +0100

    Added Dockerfile and yaml

--- src/main/java/com/example/productstore/ProductStoreApplication.java
+++ src/main/java/com/example/productstore/ProductStoreApplication.java
@@ -2,14 +2,21 @@ package com.example.productstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProductStoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductStoreApplication.class, args);
     }
 
+    @GetMapping("/message")
+    public String succesful(){
+        return "welcome";
+    }
 
 
 }
